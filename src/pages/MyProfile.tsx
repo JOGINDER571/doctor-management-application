56c42c74846da4841d1014bf784afc49c757c1a3
JOGINDER571 <snshrma7@gmail.com>
2025-08-06 22:59:11 +0530

    solve issue

--- src/pages/MyProfile.tsx
+++ src/pages/MyProfile.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { assets } from "../assets/assets";
 import { useAppContext } from "../context/AppContext";
 import { UserService } from "../services/UserService";
